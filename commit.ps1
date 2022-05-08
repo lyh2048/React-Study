6858721609b7ce08c46db447007adbb1bde80952
lyh <1046818625@qq.com>
2022-05-08 15:40:57 +0800

    2022年5月8日 15:40:57

--- commit.ps1
+++
@@ -1,3 +0,0 @@
-cd "D:\code\react\my-react-app"
-git add .
-git commit -m "$((Get-Date).DateTime)"
---
+++ submit.ps1
@@ -0,0 +1,4 @@
+cd "D:\code\react\my-react-app"
+git add .
+git commit -m "$((Get-Date).DateTime)"
+git push
